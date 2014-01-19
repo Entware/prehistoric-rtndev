d5ff44c950fb128560117db82ab72288ea675cdd
Alexander Ryzhov <ryzhovau@gmail.com>
2014-01-19 15:18:15 +0000

    25volt: merged with Zyxware Wider Powercom/APC UPSs support thanks to C61@http://forum.zyxmon.org

--- feeds/rtndev/25volt/src/usb_ups_powercom_wow.c
+++ feeds/rtndev/25volt/src/usb_ups_powercom_wow.c
@@ -2,6 +2,10 @@
 **  25volt - A lightweight tool for monitoring ups POWERCOM Imperial IMD-1025AP and maybe other for FreeBSD and Linux.
 **  Copyright (C) 2009 Dmitry Schedrin <dmx@dmx.org.ru>
 **
+**  Corrected for Powercom WOW-xxxU by c61 <mail@c61.su> 2013
+**  From program version 1.0.4 Powercom BNT, IMPERIAL, SKP, WOW series are supported
+**  (see -v and -p options)
+**
 **  This program is free software; you can redistribute it and/or modify
 **  it under the terms of the GNU General Public License as published by
 **  the Free Software Foundation; either version 2 of the License, or
@@ -31,7 +35,7 @@
 #define HID_CONFIGURATION 		0x00
 
 #define IMD_VID 			0x0d9f
-#define IMD_PID				0x00a2
+#define IMD_PID				0x00a4
 
 #define USBRQ_HID_GET_REPORT    	0x01
 #define USBRQ_HID_SET_REPORT    	0x09
@@ -59,24 +63,15 @@
 #define COMMUNICATIONLOST		(1 << 10)
 #define OVERLOAD			(1 << 11)
 
-// Voltage in
-// Voltage out
-// Freq in
-// Freq out
-// battery
-// load
-// STATUS
-
-//uint16_t voltage_in;
-//uint16_t voltage_out;
-//uint8_t frequency_in;
-//uint8_t frequency_out;
 uint16_t status;
 
-char version[] ="1.0";
+char version[] ="1.0.5";
+
+int vendor = IMD_VID, product = IMD_PID;
 
 usb_dev_handle * usb_open_imd(void);
-int tflag = 0, hflag = 0, ch;
+
+int tflag = 0, hflag = 0, bflag = 0, help = 0, detach = 0, attach = 0, skip = 0, ch;
 
 unsigned char status_human_str[][30] = {
 	"Charging:",
@@ -88,7 +83,7 @@ unsigned char status_human_str[][30] = {
 	"Need replace BAT:",
 	"Voltage not regulated:",
 	"Shutdown requested:",
-	"Shutdown emitent:",
+	"Shutdown imminent:",
 	"Communication lost:",
 	"Overload:"
 };
@@ -103,9 +98,24 @@ unsigned char status_str[][30] = {
 	"need_replace_battery:",
 	"voltage_not_regulated:",
 	"shutdown_requested:",
-	"shutdown_emitent:",
+	"shutdown_imminent:",
 	"communication_lost:",
 	"overload:"
+};
+
+unsigned char status_bash_str[][30] = {
+	"charging=",
+	"discharging=",
+	"ac_present=",
+	"battery_present=",
+	"below_capacity_limit=",
+	"remain_time_expired=",
+	"need_replace_battery=",
+	"voltage_not_regulated=",
+	"shutdown_requested=",
+	"shutdown_imminent=",
+	"communication_lost=",
+	"overload="
 };
 
 usb_dev_handle* usb_open_imd(void) {
@@ -118,8 +128,8 @@ usb_dev_handle* usb_open_imd(void) {
 
 	for (bus = usb_get_busses(); bus; bus = bus->next) {
 		for (dev = bus->devices; dev; dev = dev->next) {
-			if ((dev->descriptor.idVendor == IMD_VID)
-			    && (dev->descriptor.idProduct == IMD_PID)) {
+			if ((dev->descriptor.idVendor == vendor)
+			    && (dev->descriptor.idProduct == product)) {
 				return usb_open(dev);
 			}
 		}
@@ -200,6 +210,27 @@ uint8_t send_test(usb_dev_handle *hdev) {
 	}
 }
 
+uint8_t send_boff(usb_dev_handle *hdev) {
+
+	int8_t data;
+	int ret;
+	unsigned char tmp[2] = { 0x13, 0x00 };
+
+	ret = usb_control_msg(hdev,  0x21 , 0x09, (0x03 << 8) | 0x13, 0, tmp, 2, 5000);
+
+	if (ret < 0) {
+		printf("retrieving hid report failed\n");
+		return -1;
+	} else {
+		#ifdef DEBUG
+		printf("retrieving hid report succeeded, read %d bytes\n", ret);
+		print_data(tmp, ret);
+		#endif
+		data = tmp[1];
+		return data;
+	}
+}
+
 int main(int argc, char ** argv) {
 
 	unsigned char tmp[1024];
@@ -207,20 +238,36 @@ int main(int argc, char ** argv) {
 	usb_dev_handle *hdev = NULL;
 	unsigned char onoff[2][4] = {"off\0", "on\0"};
 
-	while ((ch = getopt(argc, argv, "ht")) != -1) {
+	while ((ch = getopt(argc, argv, "ustbv:p:dah?")) != EOF) {
 		switch (ch) {
+			case 'v':	vendor = strtol(optarg,NULL,16);	break;
+			case 'p':	product = strtol(optarg,NULL,16);	break;
+			case 'u':	hflag = 1;	break;
+			case 's':	hflag = 2;	break;
+			case 't':	tflag = 1;	skip = 1;	break;
+			case 'b':	bflag = 1;	skip = 1;	break;
+			case 'd':	detach = 1;	skip = 1;	break;
+			case 'a':	attach = 1;	skip = 1;	break;
 			case 'h':
-				hflag = 1;
-				break;
-			case 't':
-				tflag = 1;
-				break;
-			case '?':
-			default:
-				printf("%s v.%s\n", argv[0], version);
-				printf("%s [-h] [-t]\n", argv[0]);
-				exit(1);
+			case '?':	help = 1;	break;
+			default:	break;
 		}
+	}
+
+	if(help)
+	{
+		printf("USB UPS Powercom WOW-xxxU monitoring, v.%s\n", version);
+		printf(" no args - get and show ups data\n");
+		printf(" -u      - get and show unformatted ups data\n");
+		printf(" -s      - get and show UPS data for bash script\n");
+		printf(" -t      - test ups\n");
+		printf(" -b      - switch off beeper (audible alarm control; experimental)\n");
+		printf(" -d      - detach kernle driver before use interface\n");
+		printf(" -a      - attach kernle driver after use interface\n");
+		printf(" -v VID  - vendor ID (default: 0d9f - Powercom)\n");
+		printf(" -p PID  - product ID (default: 00a4 - Powercom WOW)\n");
+		printf(" -h -?   - this help\n");
+		exit(0);
 	}
 
 	//usb_set_debug(4);
@@ -228,22 +275,29 @@ int main(int argc, char ** argv) {
 
 	if ((hdev = usb_open_imd()) == NULL) {
 		printf("open failed\n");
-		return 1;
+		exit(1);
 	}
-
 
 #ifdef LIBUSB_HAS_DETACH_KERNEL_DRIVER_NP
-	if(usb_detach_kernel_driver_np(hdev, 0) < 0){
+	if (detach) {
+	    if(usb_detach_kernel_driver_np(hdev, 0) < 0) {
 		; //fprintf(stderr, "Warning: could not detach kernel driver: %s\n", usb_strerror());
+	    }
 	}
 #endif 
+
+	if (usb_claim_interface(hdev, HID_INTERFACE) < 0) {
+		printf("warning: claim interface failed\n");
+	}
 
 	ret = usb_control_msg(hdev, USB_ENDPOINT_IN | USB_TYPE_STANDARD |
 			      USB_RECIP_INTERFACE, USB_REQ_GET_DESCRIPTOR,
 			 (USB_DT_HID << 8), HID_INTERFACE, tmp, 1024, 5000);
-	if (ret < 0)
+	if (ret < 0) {
 		printf("retrieving hid descriptor failed\n");
-	else {
+		ret = 1;
+		goto release;
+	} else {
 		#ifdef DEBUG
 		printf("retrieving hid descriptor succeeded, read %d bytes\n", ret);
 		print_data(tmp, ret);
@@ -252,22 +306,62 @@ int main(int argc, char ** argv) {
 
 	//USB_TYPE_CLASS | USB_RECIP_DEVICE | USB_ENDPOINT_IN, USBRQ_HID_GET_REPORT,
 
-	// 09 21 00 01 00 01 22 d0 02 
-	
+	// 09 21 00 01 00 01 22 d0 02
+
 	if (tflag) {
 		printf("Test: ");
 		ret = send_test(hdev);
 		if (ret < 0) {
 			printf("Failed\n\n");
-			return -1;
+			ret = 1;
+			goto release;
 		} else {
 			printf("OK\n\n");
 			sleep(1);
 		}
 	}
 
-	if (hflag) {
+	if (bflag) {
+		printf("Switch OFF Audible Alarm Control: ");
+		ret = send_boff(hdev);
+		if (ret < 0) {
+			printf("Failed\n\n");
+			ret = 1;
+			goto release;
+		} else {
+			printf("OK\n\n");
+			sleep(1);
+		}
+	}
 
+	if (hflag == 1) {
+		printf("%s %d\n", "voltage_in:", get_report16(hdev, UPS_REPORT_VOLTAGE_IN));
+		printf("%s %d\n", "frequency_in:", get_report8(hdev, UPS_REPORT_FREQUENCY_IN));
+		printf("%s %d\n", "voltage_out:", get_report16(hdev, UPS_REPORT_VOLTAGE_OUT));
+		printf("%s %d\n", "frequency_out:", get_report8(hdev, UPS_REPORT_FREQUENCY_OUT));
+		printf("%s %d\n", "load:", get_report8(hdev, UPS_REPORT_LOAD));
+		printf("%s %d\n", "capacity:", get_report8(hdev, UPS_REPORT_CAPACITY));
+
+		status = get_report16(hdev, UPS_REPORT_STATUS);
+
+		for (j=0; j<12; j++) {
+			printf("%s %s\n", status_str[j], onoff[status & (1 << j) && 1]);
+		}
+	} else if (hflag == 2) {
+		printf("%s%d\n", "voltage_in=", get_report16(hdev, UPS_REPORT_VOLTAGE_IN));
+		printf("%s%d\n", "frequency_in=", get_report8(hdev, UPS_REPORT_FREQUENCY_IN));
+		printf("%s%d\n", "voltage_out=", get_report16(hdev, UPS_REPORT_VOLTAGE_OUT));
+		printf("%s%d\n", "frequency_out=", get_report8(hdev, UPS_REPORT_FREQUENCY_OUT));
+		printf("%s%d\n", "load=", get_report8(hdev, UPS_REPORT_LOAD));
+		printf("%s%d\n", "capacity=", get_report8(hdev, UPS_REPORT_CAPACITY));
+
+		status = get_report16(hdev, UPS_REPORT_STATUS);
+
+		for (j=0; j<12; j++) {
+			printf("%s%d\n", status_bash_str[j], status & (1 << j) && 1);
+		}
+	} else {
+	    if(!skip) {
 		printf("%-22s %d V\n", "Voltage in:", get_report16(hdev, UPS_REPORT_VOLTAGE_IN));
 		printf("%-22s %d Hz\n", "Frequency in:", get_report8(hdev, UPS_REPORT_FREQUENCY_IN));
 		printf("%-22s %d V\n", "Voltage out:", get_report16(hdev, UPS_REPORT_VOLTAGE_OUT));
@@ -282,45 +376,24 @@ int main(int argc, char ** argv) {
 		for (j=0; j<12; j++) {
 			printf("%-22s %s\n", status_human_str[j], onoff[status & (1 << j) && 1]);
 		}
-
-	} else {
-		
-		printf("%s %d\n", "voltage_in:", get_report16(hdev, UPS_REPORT_VOLTAGE_IN));
-		printf("%s %d\n", "frequency_in:", get_report8(hdev, UPS_REPORT_FREQUENCY_IN));
-		printf("%s %d\n", "voltage_out:", get_report16(hdev, UPS_REPORT_VOLTAGE_OUT));
-		printf("%s %d\n", "frequency_out:", get_report8(hdev, UPS_REPORT_FREQUENCY_OUT));
-		printf("%s %d\n", "load:", get_report8(hdev, UPS_REPORT_LOAD));
-		printf("%s %d\n", "capacity:", get_report8(hdev, UPS_REPORT_CAPACITY));
-		
-		status = get_report16(hdev, UPS_REPORT_STATUS);
-
-		for (j=0; j<12; j++) {
-			printf("%s %s\n", status_str[j], onoff[status & (1 << j) && 1]);
-		}
+	    }
 	}
 
-	if (usb_claim_interface(hdev, HID_INTERFACE) < 0) {
-		usb_close(hdev);
-		return 1;
-	}
-
-	for (i=0; i<0; i++) {
-		ret = usb_interrupt_read(hdev, 0x81, tmp, 8, 500);
-
-		if (ret < 0) {
-			;
-		} else {
-			#ifdef DEBUG
-			printf("interrupt read succeeded, read %d bytes\n", ret);
-			print_data(tmp, ret);
-			#endif
-		}
-	}
-
-	if (usb_release_interface(hdev, HID_INTERFACE) < 0)
+	ret = 0;
+release:;
+	if (usb_release_interface(hdev, HID_INTERFACE) < 0) {
 		printf("releasing interface failed\n");
+		ret = 1;
+	}
 
+#ifdef LIBUSB_HAS_ATTACH_KERNEL_DRIVER_NP
+	if (attach) {
+	    if(usb_attach_kernel_driver_np(hdev, 0) < 0) {
+		; //fprintf(stderr, "Warning: could not attach kernel driver: %s\n", usb_strerror());
+	    }
+	}
+#endif 
 	usb_close(hdev);
 
-	return 0;
+	exit(ret ? 1 : 0);
 }
